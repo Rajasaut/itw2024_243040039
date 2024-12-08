e83e6fc30dfc81043ef839c5e04311b9bc2211da
Rajasaut <rajasaut99@gmail.com>
2024-12-08 16:42:39 +0700

    dalam tahap pengerjaan projek 1 membuat portfolio

---
+++ latihan/itw2024_project1_243040039 (portfolio)/js/main.js
@@ -0,0 +1,11 @@
+var typed = new typed(".text", {
+  strings: [
+    "Mahasiswa (UNPAS)",
+    "Suka main bola ,",
+    "& jurusan teknik informatika",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
