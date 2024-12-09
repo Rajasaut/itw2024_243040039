ec9a7a6480bab2820fc757e43e3d5559fa31f792
Rajasaut <rajasaut99@gmail.com>
2024-12-10 01:07:12 +0700

    masih dalam peroses mengerjakan projek portfolio

--- latihan/itw2024_project1_243040039 (portfolio)/js/main.js
+++ latihan/itw2024_project1_243040039 (portfolio)/js/main.js
@@ -1,11 +1,39 @@
-var typed = new typed(".text", {
-  strings: [
-    "Mahasiswa (UNPAS)",
-    "Suka main bola ,",
-    "& jurusan teknik informatika",
-  ],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true,
+let words = document.querySelectorAll(".hobi");
+words.forEach((hobi) => {
+  let letters = hobi.textContent.split("");
+  hobi.textContent = "";
+  letters.forEach((letter) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.className = "letter";
+    hobi.append(span);
+  });
 });
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let text = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(() => {
+      letter.className = "letter out";
+    }, i * 80); // Waktu jeda per huruf
+  });
+  nextWord.style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+text();
+setInterval(text, 3000);
