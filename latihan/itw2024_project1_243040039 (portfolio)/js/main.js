de14e08a98dc2fbf637c254d8fa48421e93dd9de
Rajasaut <rajasaut99@gmail.com>
2024-12-18 12:58:49 +0700

    dalam proses mengerjakan

--- latihan/itw2024_project1_243040039 (portfolio)/js/main.js
+++ latihan/itw2024_project1_243040039 (portfolio)/js/main.js
@@ -38,21 +38,36 @@ let text = () => {
 text();
 setInterval(text, 3000);
 
-// untuk bagia aktive menu
+// untuk bagia aktive menu/////////////////
 let menuLi = document.querySelectorAll("header ul li a");
 let section = document.querySelectorAll("section");
 
 function activeMenu() {
   let len = section.length;
   while (--len && window.scrollY + 97 < section[len].offsetTop) {}
-  menulis[len].classlist.add("active");
+  menuLi.forEach((sec) => sec.classList.remove("active"));
+  if (len >= 0) menuLi[len].classList.add("active");
 }
 
 activeMenu();
 window.addEventListener("scroll", activeMenu);
 
-// untuk bagian sticky menu
+// untuk bagian sticky menu//////////////////
 const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
-  header.classList.toggle("lengket", window.scrollY > 50);
+  header.classList.toggle("sticky", window.scrollY > 50);
 });
+
+// untuk bagian icon/////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menuIcon.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
