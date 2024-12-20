9022e3a7378d869c3f7a73609f34e3b32d36a8f5
Rajasaut <rajasaut99@gmail.com>
2024-12-20 20:23:39 +0700

    sudah selesai mengerjakan projek

--- latihan/itw2024_project1_243040039 (portfolio)/js/main.js
+++ latihan/itw2024_project1_243040039 (portfolio)/js/main.js
@@ -38,20 +38,6 @@ let text = () => {
 text();
 setInterval(text, 3000);
 
-// untuk bagia aktive menu/////////////////
-let menuLi = document.querySelectorAll("header ul li a");
-let section = document.querySelectorAll("section");
-
-function activeMenu() {
-  let len = section.length;
-  while (--len && window.scrollY + 97 < section[len].offsetTop) {}
-  menuLi.forEach((sec) => sec.classList.remove("active"));
-  if (len >= 0) menuLi[len].classList.add("active");
-}
-
-activeMenu();
-window.addEventListener("scroll", activeMenu);
-
 // untuk bagian sticky menu//////////////////
 const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
